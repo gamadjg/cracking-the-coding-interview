08a23e3afbea89081233131b80f0b86b79167022
gamadjg <gamadavid36@gmail.com>
2023-01-24 08:19:14 -0800

    oneWay complete

--- arrays-and-strings/oneWay.py
+++ arrays-and-strings/oneWay.py
@@ -2,43 +2,68 @@
 
 
 def oneWay(baseStr, checkStr):
-    # check for empty arguments, if empty, return false
-    # check type, if baseStr or checkStr aren't strings, return false
-    # length check, if baseStr and checkStr aren't at most 1 character length diff, return false
-    # if length check succeeded, now check to make sure multiple characters weren't edited
+    # type check, if baseStr or checkStr aren't strings, return False
+    if type(baseStr) != str or type(checkStr) != str:
+        return False
 
-    # replacement: loop through characters in both strings, count how many were different (strings are the same length)
+    # length check, if baseStr and checkStr aren't at most 1 character length diff, return False (empty strings count)
+    if len(baseStr) > len(checkStr) + 1 or len(baseStr) < len(checkStr) - 1:
+        return False
 
-    # removal: loop through the smallest string,
+    # find the smaller of the two strings to loop through
+    smallerStr = len(baseStr) if len(baseStr) < len(checkStr) else len(checkStr)
+
+    # count how many characters are different if there are any
+    diffCount = 0
+    for i in range(smallerStr):
+        if baseStr[i] != checkStr[i]:
+            diffCount += 1
+
+    # different lengths, indicating insertion or removal, but there are also character differences, return False
+    if len(baseStr) != len(checkStr) and diffCount > 0:
+        return False
+
+    # same length, more than one replacement or no replacements, return False
+    if len(baseStr) == len(checkStr) and (diffCount > 1 or diffCount == 0):
+        return False
+
     return True
 
 
 def test_oneWay():
-    # empty base and check string
-    assert oneWay("", "abc") == False
-    assert oneWay("abc", "") == False
 
     # non-string inputs
     assert oneWay(["abc"], "abc") == False
     assert oneWay(123, "abc") == False
 
-    # # one character replacement
-    # assert oneWay("abc", "abd") == True
-    # # one character removal
-    # assert oneWay("abc", "ab") == True
-    # # one character insertion
-    # assert oneWay("abc", "abcd") == True
+    # empty base or check string
+    assert oneWay("", "abc") == False
+    assert oneWay("abc", "") == False
 
-    # # multiple character replacement, fail
-    # assert oneWay("abc", "axy") == False
-    # # multiple character replacement, same characters, different order, fail
-    # assert oneWay("abc", "bca") == False
-    # # multiple character removal, fail
-    # assert oneWay("abc", "a") == False
-    # # multiple character insertion, fail
-    # assert oneWay("abc", "abcde") == False
+    # nothing was changed between both strings, fail
+    assert oneWay("abc", "abc") == False
 
-    # # multi-case failure: insert + replacement
-    # assert oneWay("abc", "axcd") == False
-    # # multi-case failure: removal + replacement
-    # assert oneWay("abc", "ax")
+    # one character replacement
+    assert oneWay("abc", "abd") == True
+    # one character removal
+    assert oneWay("abc", "ab") == True
+    # one character removal with empty strings
+    assert oneWay("a", "") == True
+    # one character insertion
+    assert oneWay("abc", "abcd") == True
+    # one character insertion with empty strings
+    assert oneWay("", "a") == True
+
+    # multiple character replacement, fail
+    assert oneWay("abc", "axy") == False
+    # multiple character replacement, same characters, different order, fail
+    assert oneWay("abc", "bca") == False
+    # multiple character removal, fail
+    assert oneWay("abc", "a") == False
+    # multiple character insertion, fail
+    assert oneWay("abc", "abcde") == False
+
+    # multi-case failure: insert + replacement
+    assert oneWay("abc", "axcd") == False
+    # multi-case failure: removal + replacement
+    assert oneWay("abc", "ax") == False
